67e571f98ed41230af9cb4aa5e520e5aaa20a7b8
Daniel Sogl <daniel@sogls.de>
2018-04-02 11:32:15 +0200

    fix(build): fix angular AOT build errors

--- src/native-http-fallback.ts
+++ src/native-http-fallback.ts
@@ -1,4 +1,3 @@
-import 'rxjs/add/observable/fromPromise';
 import 'rxjs/add/operator/switchMap';
 
 import { HttpBackend, HttpEvent, HttpRequest } from '@angular/common/http';
@@ -6,33 +5,34 @@ import { Injectable } from '@angular/core';
 import { checkAvailability } from '@ionic-native/core';
 import { Platform } from 'ionic-angular';
 import { Observable } from 'rxjs/Observable';
+import { fromPromise } from 'rxjs/observable/fromPromise';
 
 import { NativeHttpBackend } from './native-http-backend';
 import { checkExternalRequest } from './utils/check-external-request';
 
 @Injectable()
 export class NativeHttpFallback implements HttpBackend {
-    constructor(
-        private platform: Platform,
-        private cordovaHttpBackend: NativeHttpBackend,
-        private fallbackBackend: HttpBackend,
-    ) {}
+  constructor(
+    private platform: Platform,
+    private cordovaHttpBackend: NativeHttpBackend,
+    private fallbackBackend: HttpBackend
+  ) {}
 
-    handle(req: HttpRequest<any>): Observable<HttpEvent<any>> {
-        return Observable.fromPromise(this.platform.ready()).switchMap(() => {
-            /**
-             * Native HTTP Cordova plugin doesn't like local requests
-             */
-            const isExternalRequest = checkExternalRequest(req);
+  handle(req: HttpRequest<any>): Observable<HttpEvent<any>> {
+    return fromPromise(this.platform.ready()).switchMap(() => {
+      /**
+       * Native HTTP Cordova plugin doesn't like local requests
+       */
+      const isExternalRequest = checkExternalRequest(req);
 
-            if (
-                isExternalRequest &&
-                checkAvailability('cordova.plugin.http') === true
-            ) {
-                return this.cordovaHttpBackend.handle(req);
-            } else {
-                return this.fallbackBackend.handle(req);
-            }
-        });
-    }
+      if (
+        isExternalRequest &&
+        checkAvailability('cordova.plugin.http') === true
+      ) {
+        return this.cordovaHttpBackend.handle(req);
+      } else {
+        return this.fallbackBackend.handle(req);
+      }
+    });
+  }
 }
