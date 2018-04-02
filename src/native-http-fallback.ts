a547a0b62584f29536845677f5f9aec21e352074
Daniel Sogl <daniel@sogls.de>
2018-04-02 11:41:03 +0200

    Update native-http-fallback.ts

--- src/native-http-fallback.ts
+++ src/native-http-fallback.ts
@@ -1,11 +1,10 @@
-import 'rxjs/add/operator/switchMap';
-
 import { HttpBackend, HttpEvent, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { checkAvailability } from '@ionic-native/core';
 import { Platform } from 'ionic-angular';
 import { Observable } from 'rxjs/Observable';
 import { fromPromise } from 'rxjs/observable/fromPromise';
+import { switchMap } from 'rxjs/operators';
 
 import { NativeHttpBackend } from './native-http-backend';
 import { checkExternalRequest } from './utils/check-external-request';
@@ -19,20 +18,22 @@ export class NativeHttpFallback implements HttpBackend {
   ) {}
 
   handle(req: HttpRequest<any>): Observable<HttpEvent<any>> {
-    return fromPromise(this.platform.ready()).switchMap(() => {
-      /**
-       * Native HTTP Cordova plugin doesn't like local requests
-       */
-      const isExternalRequest = checkExternalRequest(req);
+    return fromPromise(this.platform.ready()).pipe(
+      switchMap(() => {
+        /**
+         * Native HTTP Cordova plugin doesn't like local requests
+         */
+        const isExternalRequest = checkExternalRequest(req);
 
-      if (
-        isExternalRequest &&
-        checkAvailability('cordova.plugin.http') === true
-      ) {
-        return this.cordovaHttpBackend.handle(req);
-      } else {
-        return this.fallbackBackend.handle(req);
-      }
-    });
+        if (
+          isExternalRequest &&
+          checkAvailability('cordova.plugin.http') === true
+        ) {
+          return this.cordovaHttpBackend.handle(req);
+        } else {
+          return this.fallbackBackend.handle(req);
+        }
+      })
+    );
   }
 }
