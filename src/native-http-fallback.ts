d2944189b92df8d65d7b86df51788d909a9e37fa
Daniel <mytechde@outlook.com>
2018-04-21 18:21:26 +0200

    refactor(): optimize rxjs import

--- src/native-http-fallback.ts
+++ src/native-http-fallback.ts
@@ -4,36 +4,36 @@ import { checkAvailability } from '@ionic-native/core';
 import { Platform } from 'ionic-angular';
 import { Observable } from 'rxjs/Observable';
 import { fromPromise } from 'rxjs/observable/fromPromise';
-import { switchMap } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators/switchMap';
 
 import { NativeHttpBackend } from './native-http-backend';
 import { checkExternalRequest } from './utils/check-external-request';
 
 @Injectable()
 export class NativeHttpFallback implements HttpBackend {
-  constructor(
-    private platform: Platform,
-    private cordovaHttpBackend: NativeHttpBackend,
-    private fallbackBackend: HttpBackend
-  ) {}
+    constructor(
+        private platform: Platform,
+        private cordovaHttpBackend: NativeHttpBackend,
+        private fallbackBackend: HttpBackend,
+    ) {}
 
-  handle(req: HttpRequest<any>): Observable<HttpEvent<any>> {
-    return fromPromise(this.platform.ready()).pipe(
-      switchMap(() => {
-        /**
-         * Native HTTP Cordova plugin doesn't like local requests
-         */
-        const isExternalRequest = checkExternalRequest(req);
+    handle(req: HttpRequest<any>): Observable<HttpEvent<any>> {
+        return fromPromise(this.platform.ready()).pipe(
+            switchMap(() => {
+                /**
+                 * Native HTTP Cordova plugin doesn't like local requests
+                 */
+                const isExternalRequest = checkExternalRequest(req);
 
-        if (
-          isExternalRequest &&
-          checkAvailability('cordova.plugin.http') === true
-        ) {
-          return this.cordovaHttpBackend.handle(req);
-        } else {
-          return this.fallbackBackend.handle(req);
-        }
-      })
-    );
-  }
+                if (
+                    isExternalRequest &&
+                    checkAvailability('cordova.plugin.http') === true
+                ) {
+                    return this.cordovaHttpBackend.handle(req);
+                } else {
+                    return this.fallbackBackend.handle(req);
+                }
+            }),
+        );
+    }
 }
