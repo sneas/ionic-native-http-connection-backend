fb9bd561e61498c54d525de61a0ea024061cc481
xbrancher <developer@xbranch.io>
2021-11-27 13:14:50 +0100

    fix: defina custom serializor and transformer using http headers

--- src/utils/detect-serializer-and-data.ts
+++ src/utils/detect-serializer-and-data.ts
@@ -6,12 +6,46 @@ import { bodyToUtf8 } from './body-to-utf8';
 
 const DATA_REQUEST_METHODS = ['post', 'put', 'patch'];
 
+const HTTP_SERIALIZERS = ['json', 'urlencoded', 'utf8', 'multipart', 'raw'];
+const HTTP_TRANSFORMERS =['none', 'bodyToObject', 'httpParamsToFormData', 'bodyToUtf8'];
+
 export const detectSerializerAndData = (
     req: HttpRequest<any>,
 ): Partial<RequestOptions> => {
     if (!DATA_REQUEST_METHODS.includes(req.method.toLowerCase())) {
         return {
             serializer: 'urlencoded',
+        };
+    }
+
+    const serializer: string = req.headers.get('X-Http-Serializer');
+    const transformer: string = req.headers.get('X-Http-Transformer');
+
+    if (serializer && HTTP_SERIALIZERS.indexOf(serializer) === -1) {
+      throw new Error(`Serializer '${serializer}' is not allowed. Allowed serializers are: ${HTTP_SERIALIZERS.join(',')}`)
+    }
+    if (transformer && HTTP_TRANSFORMERS.indexOf(transformer) === -1) {
+        throw new Error(`Transformer '${transformer}' is not allowed. Allowed transformers are ${HTTP_TRANSFORMERS.join(',')}`)
+    }
+    if (HTTP_SERIALIZERS.indexOf(serializer) !== -1 && HTTP_TRANSFORMERS.indexOf(transformer) !== -1) {
+        let data ;
+        switch (transformer){
+            case 'none':
+                data = req.body
+                break;
+            case 'bodyToObject':
+                data = bodyToObject(req.body)
+                break;
+            case 'httpParamsToFormData':
+                data = httpParamsToFormData(req.body)
+                break;
+            case 'bodyToUtf8':
+                data = bodyToUtf8(req.body)
+                break;
+        }
+        return {
+            serializer:serializer as any,
+            data: data
         };
     }
 
